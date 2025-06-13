4a7fb48a1b289e291c550fbffe8bc04fa5898989
Héctor Fernández <hectorferca@gmail.com>
2025-06-13 11:14:28 +0000

    semaforo basico

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [color, setColor] = useState("");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	return (
+		 <div>
+		     <div className="background"></div>
+			  <div className="poste"></div>
+		     <div className = "luces">
+		         <div onClick={() => color === "rojo" ? setColor("rApagado") : setColor("rojo")} 
+				 	  className={"luz " + (color === "rojo" ? "rojo" : "rApagado" )}></div>
+
+		         <div onClick={() => color === "amarillo" ? setColor("aApagado") : setColor("amarillo")} 
+				 	  className={"luz " + (color === "amarillo" ? "amarillo" : "aApagado" )}></div>
+
+		         <div onClick={() => color === "verde" ? setColor("vApagado") :setColor("verde")} 
+					  className={"luz " + (color === "verde" ? "verde" : "vApagado" )}></div>
+		     </div>
+		 </div>
 	);
 };
 
